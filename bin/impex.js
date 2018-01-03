3e0062146397719ff57f20db3aae9a08c8e890b0
waghcwb <wagh.cwb@gmail.com>
2018-01-03 00:45:24 -0200

    Adds codecov support

---
+++ bin/impex.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+
+/***
+ *  NOT READY FOR USE
+ ***/
+
+const impex = require("../lib");
+
+
+if (argv.version || argv.v) {
+  console.log('v' + require('../package.json').version)
+  exit()
+}
