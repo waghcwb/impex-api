c5c4633f7fce25c5a4728d31c39e8cba1199d29b
Wagner Souza <wagner.junior@fh.com.br>
2018-01-02 23:58:58 -0200

    Fixes Impex import syntax

--- tests/impex.test.js
+++ tests/impex.test.js
@@ -1,7 +1,6 @@
-const Impex = require("../lib").Impex;
+const Impex = require("../lib");
 
 test("Expect impex to be instance of Impex", () => {
   let impex = new Impex();
-
   expect(impex).toBeInstanceOf(Impex);
 });
